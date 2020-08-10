fabd55123d762af9d1709c0ddef1c1f1f7c1c25d
Andressa26 <andressa.ro26@gmail.com>
2020-08-10 17:54:58 -0300

    Personalização do Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FooterBase } from './styles';
+import Logo from '../../assets/img/Logo.png';
+import IconGit from '../../assets/img/icongit.png';
+import IconLinkedin from '../../assets/img/iconlinkedin.png';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-      </a>
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="TechFlix logo" />
+      </Link>
       <p>
         Orgulhosamente criado durante a
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
+        </a>
+        por Andressa Rodrigues
+      </p>
+      <p>
+        <a href="https://github.com/Andressa26/">
+          <img height="40px" src={IconGit} alt="Ícone GitHub" />
+        </a>
+        <a href="https://www.linkedin.com/in/andressa--rodrigues/">
+          <img height="40px" src={IconLinkedin} alt="Ícone Linkedin" />
         </a>
       </p>
     </FooterBase>
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
-import { Link} from 'react-router-dom';
-//import ButtonLink from './components/ButtonLink';
 
-function Menu(){
-    return(
-        <nav className="Menu">         
-            <Link to="/">
-                <img className="Logo" src={Logo} alt="TechFlix logo" />
-            </Link>
-            <Button as={Link} className="ButtonLink" to="/cadastro/video">
-                Novo Vídeo
-            </Button>
-        </nav>
-    )
+function Menu() {
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="TechFlix logo" />
+      </Link>
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo Vídeo
+      </Button>
+    </nav>
+  );
 }
 export default Menu;
